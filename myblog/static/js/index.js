97dafb23e9ec6933b5a67c662fac0ed6f3bb32f6
T S <forever.blue.moon@gmail.com>
2019-01-06 23:53:57 +0900

    heroku pushへ

--- myblog/static/js/index.js
+++ myblog/static/js/index.js
@@ -2,11 +2,11 @@ const dassimen = document.getElementById('dassimen');
 const dassimen_low = document.getElementById('modal-overlay');
 
 //ON
-dassimen.addEventListener('mouseover', () => {
-    　　dassimen_low.style.display = 'flex';
+dassimen.addEventListener('mouseenter', () => {
+    　　dassimen_low.style.visibility = 'visible';
     }, false);
     
 //OUT
 dassimen.addEventListener('mouseleave', () => {
-    　　dassimen_low.style.display = 'none';
+    　　dassimen_low.style.visibility = 'hidden';
 }, false);
