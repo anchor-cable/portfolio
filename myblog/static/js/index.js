0a0412bcda7a8ee0f6f320eb71fb771f1d278bf9
T S <forever.blue.moon@gmail.com>
2019-01-14 16:24:35 +0900

    憲法問題解決

--- myblog/static/js/index.js
+++ myblog/static/js/index.js
@@ -1,12 +1,20 @@
-const dassimen = document.getElementById('dassimen');
-const dassimen_low = document.getElementById('modal-overlay');
+const dassimen = document.getElementById("dassimen");
+const dassimen_low = document.getElementById("modal-overlay");
 
 //ON
-dassimen.addEventListener('mouseenter', () => {
-    　　dassimen_low.style.visibility = 'visible';
-    }, false);
-    
-//OUT
-dassimen.addEventListener('mouseleave', () => {
-    　　dassimen_low.style.visibility = 'hidden';
-}, false);
+dassimen.addEventListener(
+  "mouseenter",
+  () => {
+    dassimen_low.style.display = "flex";
+  },
+  false
+);
+
+// OUT
+// dassimen.addEventListener(
+//   "mouseleave",
+//   () => {
+// dassimen_low.style.visibility = "hidden";
+//   },
+//   false
+// );
