63cf9617f0fe3abed90affd1771e859c04192f64
T S <forever.blue.moon@gmail.com>
2019-01-30 22:07:18 +0900

    上下padding取得メソッドの修正

--- myblog/static/js/revise.js
+++ myblog/static/js/revise.js
@@ -1,13 +1,10 @@
-let headerHeight = 0;
-let footerHeight = 0;
-const overlay = document.getElementById("common_overlay");
-
-const reviseMargin = () => {
-  headerHeight = document.getElementsByTagName("header")[0].clientHeight;
-  // footerHeight = document.getElementsByTagName("footer")[0].clientHeight;
-  overlay.style.marginTop = headerHeight + "px";
-  // overlay.style.marginBottom = footerHeight;
+const revisePadding = () => {
+  const headerHeight = document.getElementsByTagName("header")[0].clientHeight;
+  const footerHeight = document.getElementsByTagName("footer")[0].clientHeight;
+  const overlay = document.getElementById("common_overlay");
+  overlay.style.paddingTop = headerHeight + "px";
+  overlay.style.paddingBottom = footerHeight + "px";
 };
 
-window.onload = reviseMargin();
-window.onresize = reviseMargin();
+window.onload = revisePadding;
+window.onresize = revisePadding;
